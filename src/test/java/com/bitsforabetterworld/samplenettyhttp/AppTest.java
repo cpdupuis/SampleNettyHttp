16f86f94cc161c653f20e71aa987e170a9ec9723
Chris DuPuis <cpdupuis@gmail.com>
2019-04-13 12:13:30 -0700

    Running hello world

--- src/test/java/com/bitsforabetterworld/samplenettyhttp/AppTest.java
+++ src/test/java/com/bitsforabetterworld/samplenettyhttp/AppTest.java
@@ -10,7 +10,5 @@ import org.junit.jupiter.api.Test;
 public class AppTest {
     @Test 
     public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
     }
 }
